4128722f39a697f05dd98274615cd224a7a72463
Jayden Charbonneau <BinaryByte.DEV@gmail.com>
2020-07-19 09:06:17 -0400

    Add level 8

---
+++ src/Shared/LevelConfigs/Level8.lua
@@ -0,0 +1,9 @@
+local ServerStorage = game:GetService("ServerStorage")
+
+return {
+	Name = "We'll cross that bridge when we get there",
+	Map = ServerStorage.Assets.Levels.Level8,
+	Music = "rbxassetid://1836077465",
+	LightingState = "Level8",
+	StartingPerspective = "3D"
+}
