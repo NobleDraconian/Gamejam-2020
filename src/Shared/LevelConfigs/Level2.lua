53c1723939ec909b66cbbe8d809249d1ca2182ba
Jayden Charbonneau <BinaryByte.DEV@gmail.com>
2020-07-19 02:36:12 -0400

    Add level 2

---
+++ src/Shared/LevelConfigs/Level2.lua
@@ -0,0 +1,9 @@
+local ServerStorage = game:GetService("ServerStorage")
+
+return {
+	Name = "Fill the gap",
+	Map = ServerStorage.Assets.Levels.Level2,
+	Music = "rbxassetid://1836077465",
+	LightingState = "Level2",
+	StartingPerspective = "3D"
+}
