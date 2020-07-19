46f1130d3a6ad6ce58adfaf1792083a8f0012ad4
Jayden Charbonneau <BinaryByte.DEV@gmail.com>
2020-07-19 07:20:49 -0400

    Add level 7

---
+++ src/Shared/LevelConfigs/Level7.lua
@@ -0,0 +1,9 @@
+local ServerStorage = game:GetService("ServerStorage")
+
+return {
+	Name = "We'll cross that bridge when we get there",
+	Map = ServerStorage.Assets.Levels.Level7,
+	Music = "rbxassetid://1836077465",
+	LightingState = "Level7",
+	StartingPerspective = "3D"
+}
