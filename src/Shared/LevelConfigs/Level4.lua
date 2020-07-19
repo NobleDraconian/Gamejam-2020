e51d13cd476e68d1b827b0e9fa55f4dc3d7638ac
Jayden Charbonneau <BinaryByte.DEV@gmail.com>
2020-07-19 05:34:32 -0400

    Add level 4

---
+++ src/Shared/LevelConfigs/Level4.lua
@@ -0,0 +1,9 @@
+local ServerStorage = game:GetService("ServerStorage")
+
+return {
+	Name = "We'll cross that bridge when we get there",
+	Map = ServerStorage.Assets.Levels.Level4,
+	Music = "rbxassetid://1836077465",
+	LightingState = "Level4",
+	StartingPerspective = "3D"
+}
