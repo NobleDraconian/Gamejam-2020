61521d96d43da0bc5153c8beae0edbaa4223dc3d
Jayden Charbonneau <BinaryByte.DEV@gmail.com>
2020-07-19 07:20:32 -0400

    Add level 6

---
+++ src/Shared/LevelConfigs/Level6.lua
@@ -0,0 +1,9 @@
+local ServerStorage = game:GetService("ServerStorage")
+
+return {
+	Name = "We'll cross that bridge when we get there",
+	Map = ServerStorage.Assets.Levels.Level6,
+	Music = "rbxassetid://1836077465",
+	LightingState = "Level6",
+	StartingPerspective = "2D"
+}
