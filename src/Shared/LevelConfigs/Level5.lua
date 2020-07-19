c765b73bdfc519243b42081a4a46f7e1aa2dbfaa
Jayden Charbonneau <BinaryByte.DEV@gmail.com>
2020-07-19 05:34:42 -0400

    Add level 5

---
+++ src/Shared/LevelConfigs/Level5.lua
@@ -0,0 +1,9 @@
+local ServerStorage = game:GetService("ServerStorage")
+
+return {
+	Name = "We'll cross that bridge when we get there",
+	Map = ServerStorage.Assets.Levels.Level5,
+	Music = "rbxassetid://1836077465",
+	LightingState = "Level5",
+	StartingPerspective = "3D"
+}
